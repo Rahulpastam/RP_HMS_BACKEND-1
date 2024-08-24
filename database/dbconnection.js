7b0aac52f278185b36e24652b5ba8ea6ece0c97c
Rahulpastam <pastam.rahul@gmail.com>
2024-08-24 21:50:36 +0530

    mongo_uri updated

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 
 export const dbconnection = () => {
-  mongoose.connect(process.env.MONGO_URI, {
-    dbName: "HOSPITAL_MANAGEMENT_SYSTEM"
-  }).then(()=>console.log("DB Connected succesfully"))
-    .catch((e)=> console.log("ERROR: "+e))
-}
-
-
-
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "RP_HospitalManagementSystem",
+    })
+    .then(() => console.log("DB Connected succesfully"))
+    .catch((e) => console.log("ERROR: " + e));
+};
