2842795cde5c4a1924d6e53b970c8892c95c967f
Rahulpastam <pastam.rahul@gmail.com>
2024-09-27 23:18:10 +0530

    "Updated dbName in mongoose connection from 'RP_HospitalManagementSystem' to 'RP_HSM-1'"

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbconnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "RP_HospitalManagementSystem",
+      dbName: "RP_HSM-1",
     })
     .then(() => console.log("DB Connected succesfully"))
     .catch((e) => console.log("ERROR: " + e));
